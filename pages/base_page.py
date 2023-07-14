83988c9a62c85ac42e2f7ab1ae0ceb4b2d12b4d4
Denrois <viscount@email.cz>
2023-07-14 21:01:42 +0200

    base_page, fix impicitly wait, add is_element_present method

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,18 @@
+from selenium.common import NoSuchElementException
+
+
 class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        self.browser.imlicitely_wait(timeout)
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, element):
+        try:
+            assert self.browser.find_element(*element)
+        except NoSuchElementException:
+            return False
+        return True

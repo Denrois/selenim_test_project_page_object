e03f6494b4b2b9ae3a9519d2113b599df785a28f
Denrois <viscount@email.cz>
2023-07-14 21:05:41 +0200

    test_main_page, add test_presence_of_login_link

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,17 @@
 from .pages.main_page import MainPage
 
 
+def test_presence_of_login_link(driver):
+    link = 'http://selenium1py.pythonanywhere.com/'
+    page = MainPage(driver, link)
+    page.open()
+    page.should_be_login_link()
+
+
 def test_guest_can_go_to_login_page(driver):
     link = 'http://selenium1py.pythonanywhere.com/'
     page = MainPage(driver, link)
     page.open()
     page.go_to_login_link()
+
 

66b53e7060bebc6bf4ee2159e44b62e32b106784
Denrois <viscount@email.cz>
2023-07-14 21:04:21 +0200

    main_page, add should_be_login_link method

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,5 +3,9 @@ from .locators import MainPageLocators
 
 
 class MainPage(BasePage):
+    def should_be_login_link(self):
+        assert self.is_element_present(MainPageLocators.LOGIN_LINK)
+
     def go_to_login_link(self):
         self.browser.find_element(*MainPageLocators.LOGIN_LINK).click()
+

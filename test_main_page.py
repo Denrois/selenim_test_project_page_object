b8ba7b868152e6a8077412658a7fc3958ebc36a6
Denrois <viscount@email.cz>
2023-07-17 10:06:37 +0200

    test_main_page, add steps to reproduce and excpected results description

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,11 @@
+import pytest
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 
 def test_presence_of_login_link(driver):
+    """ 1. Open main page http://selenium1py.pythonanywhere.com/
+        Expected result: user can see 'login or register' link """
     link = 'http://selenium1py.pythonanywhere.com/'
     page = MainPage(driver, link)
     page.open()
@@ -10,6 +13,10 @@ def test_presence_of_login_link(driver):
 
 
 def test_guest_can_go_to_login_page(driver):
+    """ 1. Open main page
+        2. Click on 'login or register' link
+        Expected result: user can see login page http://selenium1py.pythonanywhere.com/accounts/login/
+        with login and register forms """
     link = 'http://selenium1py.pythonanywhere.com/'
     page = MainPage(driver, link)
     page.open()

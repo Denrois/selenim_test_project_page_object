f2be0bc5242899ce0c4fd62462df03ee8e919fc1
Denrois <viscount@email.cz>
2023-07-17 16:52:53 +0200

    locators, create class ProductPageLocators with add_to_basket, product_name, name_added_to_basket, price_before_adding, price_after_adding, success_message locators

--- pages/locators.py
+++ pages/locators.py
@@ -12,3 +12,12 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    PRODUCT_NAME = (By.CSS_SELECTOR, 'div h1')
+    NAME_ADDED_TO_BASKET = (By.CSS_SELECTOR, '#messages div:nth-child(1) strong')
+    PRICE_BEFORE_ADDING = (By.CSS_SELECTOR, '.product_main p.price_color')
+    PRICE_AFTER_ADDING = (By.CSS_SELECTOR, '.alertinner>p>strong')
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, '#messages .alert-success:nth-child(1) .alertinner')

21c7c291af1b61e7edb0ee1969d552d35bff7c7f
Denrois <viscount@email.cz>
2023-07-18 11:17:12 +0200

    product_page, test_product_page, add simplified quiz to product page, add parametrized test_guest_can_add_product_to_basket_with_0_to_9_promo to test product page

--- pages/product_page.py
+++ pages/product_page.py
@@ -39,3 +39,10 @@ class ProductPage(BasePage):
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def solve_the_quiz_simplified(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
--- test_product_page.py
+++ test_product_page.py
@@ -1,3 +1,4 @@
+import pytest
 from .pages.product_page import ProductPage
 
 
@@ -20,4 +21,17 @@ def test_guest_can_add_product_to_basket(driver):
     page.product_price_match()
 
 
-
+@pytest.mark.parametrize('promo', [el if el !=7 else pytest.param(el, marks=pytest.mark.xfail(
+                                    reason='known bug with name mismatch in promo7')) for el in range(10)])
+def test_guest_can_add_product_to_basket_with_0_to_9_promo(driver, promo):
+    """ Repeat the previous test(test_guest_can_add_product_to_basket) 10 times
+        with different promo codes(from promo0 to promo9)
+        Expected result: all tests should pass, except test with promo7 """
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo}'
+    page = ProductPage(driver, link)
+    page.open()
+    page.should_be_add_to_basket_button()
+    page.add_to_basket()
+    page.solve_the_quiz_simplified()
+    page.product_name_match()
+    page.product_price_match()

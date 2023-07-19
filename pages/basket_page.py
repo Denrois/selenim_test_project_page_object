0b61f81b16d8e26bfb5f9fdf36cc5ffbfe165cb5
Denrois <viscount@email.cz>
2023-07-19 11:45:00 +0200

    basket_page, create class BasketPage with presence_of_empty_basket_message and no_items_in_basket methods

---
+++ pages/basket_page.py
@@ -0,0 +1,12 @@
+from .base_page import BasePage
+from .locators import BasketPageLocators
+
+
+class BasketPage(BasePage):
+    def presence_of_empty_basket_message(self):
+        assert self.is_element_present(BasketPageLocators.EMPTY_BASKET_MSG), \
+            'No empty basket message'
+
+    def no_items_in_basket(self):
+        assert self.is_element_not_present(BasketPageLocators.ITEMS_IN_BASKET), \
+            'Basket is not empty'

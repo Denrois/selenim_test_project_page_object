af0e9cad4dec875026a36ad12a7cc49dd934a3fd
Denrois <viscount@email.cz>
2023-07-19 13:13:22 +0200

    test_product_page, pytest.ini, add login_guest mark to pytest.ini; create testsuit TestLoginGuest for test_presence_of_login_link and test_guest_can_go_to_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,27 +1,30 @@
+import pytest
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 
-def test_presence_of_login_link(driver):
-    """ 1. Open main page http://selenium1py.pythonanywhere.com/
-        Expected result: user can see 'login or register' link """
-    link = 'http://selenium1py.pythonanywhere.com/'
-    page = MainPage(driver, link)
-    page.open()
-    page.should_be_login_link()
 
+@pytest.mark.login_guest
+class TestLoginGuest:
+    def test_presence_of_login_link(self, driver):
+        """ 1. Open main page http://selenium1py.pythonanywhere.com/
+            Expected result: user can see 'login or register' link """
+        link = 'http://selenium1py.pythonanywhere.com/'
+        page = MainPage(driver, link)
+        page.open()
+        page.should_be_login_link()
 
-def test_guest_can_go_to_login_page(driver):
-    """ 1. Open main page http://selenium1py.pythonanywhere.com/
-        2. Click on 'login or register' link
-        Expected result: user can see login page http://selenium1py.pythonanywhere.com/accounts/login/
-        with login and register forms """
-    link = 'http://selenium1py.pythonanywhere.com/'
-    page = MainPage(driver, link)
-    page.open()
-    page.go_to_login_link()
-    login_page = LoginPage(driver, driver.current_url)
-    login_page.should_be_login_page()
+    def test_guest_can_go_to_login_page(self, driver):
+        """ 1. Open main page http://selenium1py.pythonanywhere.com/
+            2. Click on 'login or register' link
+            Expected result: user can see login page http://selenium1py.pythonanywhere.com/accounts/login/
+            with login and register forms """
+        link = 'http://selenium1py.pythonanywhere.com/'
+        page = MainPage(driver, link)
+        page.open()
+        page.go_to_login_link()
+        login_page = LoginPage(driver, driver.current_url)
+        login_page.should_be_login_page()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(driver):
